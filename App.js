44743ffd5c87ee66b4852c9ca2c6b91d588da48a
Suthar98 <sutharvictor10@gmail.com>
2023-01-31 21:09:47 +0530

    testing

---
+++ App.js
@@ -0,0 +1,8 @@
+export default function App( navigation ) {
+    return(
+          <View>
+
+          </View>
+          )
+
+}
---
+++ src/get_employee.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View } from "react-native";
+const getEmployee({navigation}) => {
+    return(
+        <View></View>
+
+)}
