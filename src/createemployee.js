fd26b45800a4b1d367ff0444237e9a466e14bab2
tharshi2803 <67555968+tharshi2803@users.noreply.github.com>
2023-01-31 21:09:43 +0530

    test

---
+++ src/createemployee.js
@@ -0,0 +1,18 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+
+
+const createEmployee = ({ navigation }) => {
+    return(
+        <View></View>
+    )
+}
+  
